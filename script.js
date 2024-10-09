b3aab4297b7d62d5fb7a247b895f2094a9211e34
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-09 17:21:37 +0200

    add base function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('ravanello');
+
+
+const user_nameEl = document.querySelector('.user_name')
+const user_kmEl = document.querySelector('.user_km')
+const formEl = document.querySelector('.user_form')
+
+
+formEl.addEventListener('submit', function(e) {
+    e.preventDefault()
+    console.log(e.target.user_name.value);
+    console.log(e.target.user_km.value);
+    console.log(e.target.user_age.value);
+    
+}
+)
