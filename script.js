7befd0ad0919f2b52ed40ffd4ab39f264c8accb6
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-09 19:39:56 +0200

    complete_base_function

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const user_nameEl = document.querySelector('.user_name')
 const user_kmEl = document.querySelector('.user_km')
 const formEl = document.querySelector('.user_form')
 const user_ageEl = document.querySelector('.user_age')
+const resultEl = document.getElementById('result');
 
 
 formEl.addEventListener('submit', function (e) {
     e.preventDefault()
-    console.log(e.target.user_name.value);
-    console.log(e.target.user_km.value);
+    console.log(e.target.user_name.value,);
+    console.log(e.target.user_km.value,'Km');
     console.log(e.target.user_age.value);
 
     const price_for_km = 0.21;
@@ -20,15 +21,23 @@ formEl.addEventListener('submit', function (e) {
 
     if (user_ageEl.value == "minor") {
 
-        price = price * 0.8;
+        price *= 0.8;
+        discount_message = 'Biglietto Scontato (20%)';
+        
 
     } else if (user_ageEl.value == "senior") {
 
-        price = price * 0.6;
+        price *= 0.6;
+        discount_message = 'Biglietto Scontato (40%)';
 
+    }else{
+    discount_message = 'Biglietto Standard';
     }
 
-    console.log(price.toFixed(2));
+    resultEl.innerHTML = `
+        <p>${user_nameEl.value}</p>
+        <p>€${price.toFixed(2)}</p>
+        <p>${discount_message}</p>`;
 
 }
 
